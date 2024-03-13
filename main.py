943450be1c16ee6ab0fc1ca07bbb3b2c8cede3ea
Amundw99 <amund@westin-emission.no>
2024-03-13 17:52:22 +0100

    first version

---
+++ main.py
@@ -0,0 +1,27 @@
+import RPi.GPIO as GPIO
+import time
+
+# Angi GPIO-nummeret du vil bruke
+GPIO_PIN = 18
+
+# Sett opp GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(GPIO_PIN, GPIO.OUT)
+
+try:
+    while True:
+        # Sett GPIO til høy (3.3V)
+        GPIO.output(GPIO_PIN, GPIO.HIGH)
+        print("GPIO set to HIGH")
+        time.sleep(1)  # Vent 1 sekund
+
+        # Sett GPIO til lav (0V)
+        GPIO.output(GPIO_PIN, GPIO.LOW)
+        print("GPIO set to LOW")
+        time.sleep(1)  # Vent 1 sekund
+
+except KeyboardInterrupt:
+    print("\nProgrammet avsluttet av brukeren.")
+finally:
+    # Rydd opp GPIO-innstillingene før programmet avsluttes
+    GPIO.cleanup()
